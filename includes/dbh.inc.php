45dec33de6309797d60724aa989fbc07a3e2c6ef
MiGi7 <mike.m7@hotmail.com>
2019-05-25 17:38:05 -0400

    error checkinig

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,3 +3,7 @@
 $connectionInfo = array("UID" => "mycomputer@themigiserver", "pwd" => "@Kingston2019", "Database" => "Projects", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:themigiserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+if (!$conn){
+  echo "ERROR";
+  exit();
+}
