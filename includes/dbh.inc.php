662ae90374a6a95a02aa5a03fc30fd51fb7fb19d
MiGi7 <mike.m7@hotmail.com>
2019-05-25 11:46:40 -0400

    password chnaged

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,4 +1,4 @@
 <?php
-$connectionInfo = array("UID" => "mycomputer@themigiserver", "pwd" => "{your_password_here}", "Database" => "Projects", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "mycomputer@themigiserver", "pwd" => "@Kingston2019", "Database" => "Projects", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:themigiserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
