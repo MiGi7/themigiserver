fcfdeb628e3305db70b8c8cdfc641284d5fcdeae
Ubuntu <michael@michael.hjg21cshmxlu3aacl0vgvv24oa.bx.internal.cloudapp.net>
2019-05-25 20:52:22 +0000

    changed php version

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,5 +1,6 @@
 <?php
 // SQL Server Extension Sample Code:
+
 $connectionInfo = array("UID" => "mycomputer@themigiserver", "pwd" => "@Kingston2019", "Database" => "Projects", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:themigiserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
