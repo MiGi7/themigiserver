48b730ecec9ed4554a4e7fffe214b89e7f52a8fa
MiGi7 <mike.m7@hotmail.com>
2019-05-25 14:37:40 -0400

    db

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,10 +1,5 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:themigiserver.database.windows.net,1433; Database = Projects", "mycomputer", "@Kingston2019");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "mycomputer@themigiserver", "pwd" => "@Kingston2019", "Database" => "Projects", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:themigiserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
