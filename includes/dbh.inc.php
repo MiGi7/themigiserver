dbe234683bcbf713cbf20c599a4593b8c9a868bd
MiGi7 <mike.m7@hotmail.com>
2019-05-25 13:47:08 -0400

    db

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,10 @@
 <?php
-
-$dbServername = "mycomputer@themigiserver:1443";
-$dbUsername = "mycomputer@themigiserver";
-$dbPassword = "@Kingston2019";
-$dbName = "Projects";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:themigiserver.database.windows.net,1433; Database = Projects", "mycomputer", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
