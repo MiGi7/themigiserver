145853b373821d0bd36322f5ecd19987590ac53d
MiGi7 <mike.m7@hotmail.com>
2019-05-26 01:00:09 -0400

    sign in

--- includes/signin.inc.php
+++ includes/signin.inc.php
@@ -14,14 +14,14 @@ if (isset($_POST['login'])) {
    exit();
  } else {
    $sql = "SELECT * FROM logins WHERE user_name='$uid'";
-   $result = mysqli_query($conn, $sql);
-   $check = mysqli_num_rows($result);
+   $result = sqlsrv_query($conn, $sql);
+   $check = sqlsrv_has_rows($result);
    if ($check < 1) {
      header('Location: ../index.php?login=error1');
      exit();
    } else {
      //dehashing and pass check
-     if ($row = mysqli_fetch_assoc($result)) {
+     if ($row = sqlsrv_fetch_array($result)) {
        $hashedpasscheck = password_verify($pass, $row['user_pass']);
        if ($hashedpasscheck == false) {
          header('Location: ../index.php?login=error1');
